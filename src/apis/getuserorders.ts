5fa4245e1900c692d66b9a24e0f44b72f5a36e02
mohamedeltonbary <mohamedeltonbary5@gmail.com>
2025-09-23 04:27:44 +0300

    first commit

--- src/apis/getuserorders.ts
+++ src/apis/getuserorders.ts
@@ -1,3 +1,22 @@
+// "use server"
+
+// import { getMyToken } from "@/utilites/token"
+// import axios from "axios"
+// import { jwtDecode } from "jwt-decode"
+
+
+// export async function getuserorder() {
+//     const token = await getMyToken()
+//     const { id } = jwtDecode(token)
+//     if (!token) {
+//         throw new Error("not found")
+//     }
+//     const data = await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${id}`)
+//     return data
+// }
+
+
+
 "use server"
 
 import { getMyToken } from "@/utilites/token"
--- src/auth.ts
+++ src/auth.ts
@@ -1,63 +1,66 @@
 
-import { AuthOptions } from 'next-auth';
-import CredentialsProvider from "next-auth/providers/credentials";
+// import { AuthOptions } from 'next-auth';
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
 
-export const authOptions: AuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" },
-            },
-            authorize: async (credentials) => {
-                const response = await fetch(`${process.env.API}/auth/signin`, {
-                    method: "POST",
-                    body: JSON.stringify({
-                        email: credentials?.email,
-                        password: credentials?.password,
-                    }),
-                    headers: { "Content-Type": "application/json" },
-                });
-                const payload = await response.json();
-                console.log('Response status:', response.status);
-                console.log(payload);
-                if (payload.message === "success") {
-                    const { id }: { id:string } = jwtDecode(payload.token);
 
-                    // console.log(id); 
-                    return {
-                        id: id,
-                        user: payload.user,
-                        token: payload.token
-                    };
-                }
-                throw new Error(payload.message || "Invalid credentials");
 
-                //  return ;
-            },
-        }),
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.user = user?.user
-                token.token = user?.token
-            }
-            return token
-        },
 
-          async session({ session, token }) {
-            if (token) {
-                session.user = token?.user
+// export const authOptions: AuthOptions = {
+//     providers: [
+//         CredentialsProvider({
+//             name: 'Credentials',
+//             credentials: {
+//                 email: { label: "Username", type: "text", placeholder: "jsmith" },
+//                 password: { label: "Password", type: "password" },
+//             },
+//             authorize: async (credentials) => {
+//                 const response = await fetch(`${process.env.API}/auth/signin`, {
+//                     method: "POST",
+//                     body: JSON.stringify({
+//                         email: credentials?.email,
+//                         password: credentials?.password,
+//                     }),
+//                     headers: { "Content-Type": "application/json" },
+//                 });
+//                 const payload = await response.json();
+//                 console.log('Response status:', response.status);
+//                 console.log(payload);
+//                 if (payload.message === "success") {
+//                     const { id }: { id:string } = jwtDecode(payload.token);
 
-            }
-            return session
-        }
-    }
-}
+//                     // console.log(id); 
+//                     return {
+//                         id: id,
+//                         user: payload.user,
+//                         token: payload.token
+//                     };
+//                 }
+//                 throw new Error(payload.message || "Invalid credentials");
+
+//                 //  return ;
+//             },
+//         }),
+//     ],
+//     callbacks: {
+//         async jwt({ token, user }) {
+//             if (user) {
+//                 token.user = user?.user
+//                 token.token = user?.token
+//             }
+//             return token
+//         },
+
+//           async session({ session, token }) {
+//             if (token) {
+//                 session.user = token?.user
+
+//             }
+//             return session
+//         }
+//     }
+// }
 
 
 
@@ -123,3 +126,88 @@ export const authOptions: AuthOptions = {
 //     }
 // };
 
+
+
+
+
+
+
+
+
+
+
+import { AuthOptions } from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials";
+
+// import { jwtDecode } from "jwt-decode";
+import { jwtDecode, JwtPayload } from "jwt-decode";
+interface MyToken extends JwtPayload {
+  id: string;
+}
+
+
+export async function getuserorder() {
+  const token = await getMyToken();
+  const { id } = jwtDecode<MyToken>(token);
+
+  if (!token) {
+    throw new Error("not found");
+  }
+
+  return id;
+}
+
+export const authOptions: AuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" },
+            },
+            authorize: async (credentials) => {
+                const response = await fetch(`${process.env.API}/auth/signin`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: credentials?.email,
+                        password: credentials?.password,
+                    }),
+                    headers: { "Content-Type": "application/json" },
+                });
+                const payload = await response.json();
+                console.log('Response status:', response.status);
+                console.log(payload);
+                if (payload.message === "success") {
+                    const { id }: { id:string } = jwtDecode(payload.token);
+
+                    // console.log(id); 
+                    return {
+                        id: id,
+                        user: payload.user,
+                        token: payload.token
+                    };
+                }
+                throw new Error(payload.message || "Invalid credentials");
+
+                //  return ;
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = user?.user
+                token.token = user?.token
+            }
+            return token
+        },
+
+          async session({ session, token }) {
+            if (token) {
+                session.user = token?.user
+
+            }
+            return session
+        }
+    }
+}
