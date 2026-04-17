d26987fbdfa228e440d1e78317ecc107428ed26f
Mohamed Eltonbary <mohamedeltonbary5@gmail.com>
2026-04-17 19:33:50 +0200

    gsfg

--- src/paymentAction/onlinePayment.ts
+++ src/paymentAction/onlinePayment.ts
@@ -1,3 +1,24 @@
+// "use server";
+
+// import { getMyToken } from "@/utilites/token";
+// import axios from "axios";
+
+// export async function onlinePaymentAction(id: string, value: object) {
+//   const token = await getMyToken();
+  // console.log(token)
+//   if (!token) {
+//     throw new Error("login first");
+//   }
+//   const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, value, {
+//       headers: {
+//           token: token as string
+//       }
+//   });
+
+//   return data;
+// }
+
+
 "use server";
 
 import { getMyToken } from "@/utilites/token";
@@ -5,24 +26,22 @@ import axios from "axios";
 
 export async function onlinePaymentAction(id: string, value: object) {
   const token = await getMyToken();
-  // console.log(token)
+
   if (!token) {
     throw new Error("login first");
   }
-  // const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, value, {
-  //     headers: {
-  //         token: token as string
-  //     }
-  // });
+
+  console.log("Checkout session URL:", `${process.env.NEXT_URL}/payment`);
 
   const { data } = await axios.post(
-    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${process.env.NEXT_URL}/cart`,
+    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${process.env.NEXT_URL}/payment`,
     value,
     {
       headers: {
         token: token as string,
       },
-    },
+    }
   );
+
   return data;
 }
