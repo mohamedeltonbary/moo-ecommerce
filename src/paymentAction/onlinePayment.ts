d740f65082c679e6cad13e67b78a81ff03486b48
Mohamed Eltonbary <mohamedeltonbary5@gmail.com>
2026-04-17 20:30:14 +0200

    fix online payment redirect

--- src/paymentAction/onlinePayment.ts
+++ src/paymentAction/onlinePayment.ts
@@ -1,3 +1,26 @@
+// "use server";
+
+// import { getMyToken } from "@/utilites/token";
+// import axios from "axios";
+
+// export async function onlinePaymentAction(id: string, value: object) {
+//   const token = await getMyToken();
+  // console.log(token)
+//   if (!token) {
+//     throw new Error("login first");
+//   }
+//   const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, value, {
+//       headers: {
+//           token: token as string
+//       }
+//   });
+
+//   return data;
+// }
+
+
+
+
 "use server";
 
 import { getMyToken } from "@/utilites/token";
@@ -5,15 +28,20 @@ import axios from "axios";
 
 export async function onlinePaymentAction(id: string, value: object) {
   const token = await getMyToken();
-  // console.log(token)
+
   if (!token) {
     throw new Error("login first");
   }
-  const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, value, {
+
+  const { data } = await axios.post(
+    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${process.env.NEXT_URL}`,
+    value,
+    {
       headers: {
-          token: token as string
-      }
-  });
+        token: token as string,
+      },
+    }
+  );
 
   return data;
 }
