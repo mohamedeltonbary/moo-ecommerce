766f59177303c25aeea31c0d29d3fedf6160d620
Mohamed Eltonbary <mohamedeltonbary5@gmail.com>
2026-04-17 17:34:42 +0200

    تعديل URL الدفع باستخدام NEXT_URL

--- src/paymentAction/onlinePayment.ts
+++ src/paymentAction/onlinePayment.ts
@@ -1,18 +1,28 @@
-"use server"
+"use server";
 
-import { getMyToken } from "@/utilites/token"
-import axios from "axios"
+import { getMyToken } from "@/utilites/token";
+import axios from "axios";
 
 export async function onlinePaymentAction(id: string, value: object) {
-    const token = await getMyToken()
-    // console.log(token)
-    if (!token) {
-        throw new Error("login first")
-    }
-    const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, value, {
-        headers: {
-            token: token as string
-        }
-    });
-    return data
-} 
+  const token = await getMyToken();
+  // console.log(token)
+  if (!token) {
+    throw new Error("login first");
+  }
+  // const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, value, {
+  //     headers: {
+  //         token: token as string
+  //     }
+  // });
+
+  const { data } = await axios.post(
+    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${process.env.NEXT_URL}/cart`,
+    value,
+    {
+      headers: {
+        token: token as string,
+      },
+    },
+  );
+  return data;
+}
